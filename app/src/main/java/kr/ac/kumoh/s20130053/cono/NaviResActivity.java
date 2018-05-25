e4d49a8298d25185251a7399b6b59d87606dfcd4
곽중호 <thdge8@gmail.com>
2018-05-25 16:44:17 +0900

    v0.03.001

--- app/src/main/java/kr/ac/kumoh/s20130053/cono/NaviResActivity.java
+++ app/src/main/java/kr/ac/kumoh/s20130053/cono/NaviResActivity.java
@@ -4,13 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,7 +21,7 @@ import static kr.ac.kumoh.s20130053.cono.MainActivity.db;
 import static kr.ac.kumoh.s20130053.cono.MainActivity.hairshop_name;
 import static kr.ac.kumoh.s20130053.cono.MainActivity.hairshop_token;
 
-public class NaviResActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class NaviResActivity extends AppCompatActivity {
 
     private CalendarView mCalendarView;
     private Spinner mSpinner;
@@ -68,42 +65,11 @@ public class NaviResActivity extends AppCompatActivity implements AdapterView.On
         mAdapter = ArrayAdapter.createFromResource(this, R.array.TimeSpinner, android.R.layout.simple_spinner_dropdown_item);
         mSpinner = findViewById(R.id.nav_res_TimeSpinner);
         mSpinner.setAdapter(mAdapter);
-        mSpinner.setOnItemSelectedListener(this);
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
         // 이부분 수정해야함
         mDesignerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, mArray);
         mSpinner = findViewById(R.id.nav_res_DesignerSpinner);
         mSpinner.setAdapter(mDesignerAdapter);
-        mSpinner.setOnItemSelectedListener(this);
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch (view.getId()) {
-            case R.id.nav_res_TimeSpinner:
-                Toast.makeText(NaviResActivity.this, "핡", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.nav_res_DesignerSpinner:
-                Toast.makeText(NaviResActivity.this, "핡", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
     }
 
     public int getCurrentYear() {
