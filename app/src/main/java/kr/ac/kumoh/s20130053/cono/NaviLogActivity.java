75c66fde07519638bee13f064c0b7fb33d00f069
곽중호 <thdge8@gmail.com>
2018-06-09 01:10:25 +0900

    v0.30

--- app/src/main/java/kr/ac/kumoh/s20130053/cono/NaviLogActivity.java
+++ app/src/main/java/kr/ac/kumoh/s20130053/cono/NaviLogActivity.java
@@ -16,6 +16,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 import static kr.ac.kumoh.s20130053.cono.MainActivity.db;
 import static kr.ac.kumoh.s20130053.cono.MainActivity.hairshop_name;
@@ -25,6 +26,7 @@ import static kr.ac.kumoh.s20130053.cono.SignInActivity.mAuth;
 public class NaviLogActivity extends AppCompatActivity {
 
     private TableLayout tableLayout;
+    private ArrayList<TableRow> mArray;
     private int amount;
 
     @Override
@@ -34,7 +36,11 @@ public class NaviLogActivity extends AppCompatActivity {
         if (hairshop_name != null)
             getSupportActionBar().setTitle(R.string.log);
 
-        amount = 0; // 결제 누적액 초기화
+        // 결제 누적액 초기화
+        amount = 0;
+
+        // TableRow 정렬을 위한 객체 초기화
+        mArray = new ArrayList<>();
 
         // TableLayout 객체 획득
         tableLayout = findViewById(R.id.nav_log_tableLayout);
@@ -46,24 +52,43 @@ public class NaviLogActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (DocumentSnapshot document : task.getResult()) {
-                                if (String.valueOf(document.getData().get("ID")).equals(mAuth.getCurrentUser().getEmail()))
-                                    DynamicTableRowInsert(
+                                if (String.valueOf(document.getData().get("ID")).equals(mAuth.getCurrentUser().getEmail())) {
+                                    mArray.add(DynamicTableRowCreator(
                                             String.valueOf(document.getData().get("date")),
                                             String.valueOf(document.getData().get("designer")),
                                             String.valueOf(document.getData().get("Cut")),
                                             String.valueOf(document.getData().get("Payment_Method")),
-                                            String.valueOf(document.getData().get("Money")));
+                                            String.valueOf(document.getData().get("Money"))));
+                                }
                             }
-
-                            // 여기서 날짜별로 정렬하는 코드 삽입해야함
+                            // 누적결제액 표기
                             Toast.makeText(NaviLogActivity.this, "누적결제액 : " + CurrencyConverse(amount) + "원", Toast.LENGTH_LONG).show();
+
+                            // 동적으로 생성한 TableRow 뷰를 날짜별로 정렬해주는 코드
+                            Date date = new Date();
+                            TableRow tmp;
+                            for (int i = 0; i < mArray.size() - 1; i++)
+                                for (int j = i + 1; j < mArray.size(); j++) {
+                                    ((TextView) (mArray.get(i).getChildAt(0))).getText().toString(); // 날짜정보
+                                    if (date.CompareLatestDate(
+                                            ((TextView) (mArray.get(i).getChildAt(0))).getText().toString(),
+                                            ((TextView) (mArray.get(j).getChildAt(0))).getText().toString())) {
+                                        tmp = mArray.get(i);
+                                        mArray.set(i, mArray.get(j));
+                                        mArray.set(j, tmp);
+                                    }
+                                }
+
+                            // TableLayout 에 정렬이 끝난 TableRow 를 삽입
+                            for (int i = 0; i < mArray.size(); i++)
+                                tableLayout.addView(mArray.get(i));
                         } else
                             Log.d("TAG", "Error getting documents: ", task.getException());
                     }
                 });
     }
 
-    protected void DynamicTableRowInsert(String date, String designer, String type, String paymentMethod, String paymentAmount) {
+    protected TableRow DynamicTableRowCreator(String date, String designer, String type, String paymentMethod, String paymentAmount) {
         // 새로 추가될 TableRow(행) 객체 생성
         TableRow tableRow = new TableRow(this);
 
@@ -112,11 +137,10 @@ public class NaviLogActivity extends AppCompatActivity {
         tableRow.addView(textView4, ItemStyle);
         tableRow.addView(textView5, ItemStyle);
 
-        // TableRow 를 TableLayout 에 삽입
-        tableLayout.addView(tableRow);
-
         // 누적결제액 계산
         amount += Integer.valueOf(paymentAmount);
+
+        return tableRow;
     }
 
     protected String CurrencyConverse(int value) {
